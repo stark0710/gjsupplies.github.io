79ac8ecc6e5be10ddf1eca6388dc9f144efdf90a
stark0710 <mguru493@gmail.com>
2026-02-09 03:37:50 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -2,7 +2,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
 // TODO: REPLACE WITH YOUR ACTUAL SUPABASE PROJECT CREDENTIALS
-const SUPABASE_URL = 'https://hbjvatsjsgmrgzprbwqj.supabase.co';
+const SUPABASE_URL = 'https://gjsupplies.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhianZhdHNqc2dtcmd6cHJid3FqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1NTM2ODEsImV4cCI6MjA4NjEyOTY4MX0.WcCFHPSqPaUy3brOKx2XAPn3vPqDQoi1JhHOyaGgKvg';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
