5b51c9def34c30a41eb827a741d3107063059457
stark0710 <mguru493@gmail.com>
2026-03-16 19:07:59 +0530

    Update orders.js

--- js/orders.js
+++ js/orders.js
@@ -1,6 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+import { getFirestore, collection, query, where, getDocs, orderBy } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
 
-import { supabase } from './supabase.js';
+const firebaseConfig = {
+  apiKey: "AIzaSyCeb0B0Qb8c5qdo0vQuQ4kIBTy2NdMCdmA",
+  authDomain: "gjsupplies-efe10.firebaseapp.com",
+  projectId: "gjsupplies-efe10",
+  storageBucket: "gjsupplies-efe10.firebasestorage.app",
+  messagingSenderId: "94847411867",
+  appId: "1:94847411867:web:1879c5000215c95960083e"
+};
 
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 const myOrdersList = document.getElementById('my-orders-list');
 const loginBtn = document.getElementById('login-btn');
 
@@ -14,20 +25,26 @@ loginBtn.addEventListener('click', () => {
 });
 
 async function fetchMyOrders(email) {
-    myOrdersList.innerHTML = '<p>Loading your orders...</p>';
 
-    try {
-        const { data: orders, error } = await supabase
-            .from('orders')
-            .select('*')
-            .eq('customer_email', email)
-            .order('created_at', { ascending: false });
-
-        if (error) throw error;
-
-        renderMyOrders(orders);
-
-    } catch (err) {
+    myOrdersList.innerHTML = "Loading your orders...";
+    
+    const q = query(
+    collection(db, "orders"),
+    where("customer_email", "==", email),
+    orderBy("orderTime", "desc")
+    );
+    
+    const querySnapshot = await getDocs(q);
+    
+    let orders = [];
+    
+    querySnapshot.forEach((doc) => {
+    orders.push({ id: doc.id, ...doc.data() });
+    });
+    
+    renderMyOrders(orders);
+    
+}catch (err) {
         console.error('Error fetching your orders:', err);
         myOrdersList.innerHTML = '<p>Error loading orders.</p>';
     }
